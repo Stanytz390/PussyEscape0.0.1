8b58fa0045fb2229bb6b28ddbabeecd97ac5fcd8
Stany Max Hub <stanytz953@gmail.com>
2026-07-28 06:39:51 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+global.sessionid = process.env.SESSION_ID || '';
+global.BOT_PREFIX = '.';
+global.owners = ['255620490076@lid', ''];
+global.dev = ['255620490076@s.whatsapp.net','25770239992037@lid'];
+global.menuImage = 'https://url.bmbxmd.workers.dev/Migo.jpeg';
+global.ownerName = 'ꜱᴛᴀɴʏᴛᴢ';
