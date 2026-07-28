b022629b11082e0a36fe223e89270f4aba5774bd
Stany Max Hub <stanytz953@gmail.com>
2026-07-28 12:58:30 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 global.sessionid = process.env.SESSION_ID || '';
 global.BOT_PREFIX = '.';
-global.owners = ['75519081111709:2@lid', ''];
+global.owners = ['22248584912928:33@lid', ''];
 global.dev = ['255611858502:2@s.whatsapp.net','75519081111709@lid'];
 global.menuImage = 'https://url.bmbxmd.workers.dev/Migo.jpeg';
 global.ownerName = 'ꜱᴛᴀɴʏᴛᴢ';
